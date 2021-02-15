99d148e53c3d1c35ac5be7a63a56e687b7ef3acc
Вахрушев Антон Геннадьевич <AGVakhrushev@sberbank.ru>
2021-02-15 19:58:10 +0300

    Rewrite SQL module to simplify queries. Add customization in query params

--- autowoe/lib/utilities/sql.py
+++ autowoe/lib/utilities/sql.py
@@ -1,134 +1,278 @@
-from typing import Dict, List
-from io import StringIO
+from typing import Optional
 
 
-def preprocess_features(model_data: Dict, table_name: str) -> List[str]:
+def prepare_number(woe_dict, name: str, r_val: int = 3, round_features: int = 5,
+                   nan_pattern: str = "({0} IS NULL OR {0} = 'NaN')",
+                   preprocessing: Optional[str] = None):
+    """
+    Get encoding case when for number
+
+    Args:
+        woe_dict:
+        name:
+        r_val:
+        round_features:
+        nan_pattern:
+        preprocessing:
+
+    Returns:
+
+    """
+    # value in case
+    f_val = name
+    if preprocessing is not None:
+        f_val = preprocessing.format(name)
+
+    # search for NaN encoding
+    for grp in woe_dict.cod_dict:
+        if type(grp) is str and grp.startswith('__NaN_'):
+            nan_val = round(woe_dict.cod_dict[grp], r_val)
+            break
+    else:
+        raise ValueError('NaN encoding value does not exists in woe_dict')
+
+    nan_case = nan_pattern.format(f_val)
+    feature = """CASE\n  WHEN {0} THEN {1}\n""".format(nan_case, nan_val)
+    # create regular bins
+    for grp, val in enumerate(woe_dict.split):
+        enc_val = round(woe_dict.cod_dict[grp], r_val)
+        feature += """  WHEN {0} <= {1} THEN {2}\n""".format(f_val, round(val, round_features), enc_val)
+    # create last else val
+    enc_val = round(woe_dict.cod_dict[grp + 1], r_val)
+    feature += """  ELSE {1}\nEND AS {0}""".format(name, enc_val, )
+
+    return feature
+
+
+def check_cat_symb(x: str):
     """
 
     Args:
-        model_data:
+        x:
+
+    Returns:
+
+    """
+    if type(x) is str:
+        x = "'{0}'".format(x)
+    else:
+        x = str(x)
+
+    return x
+
+
+def prepare_category(woe_dict, name: str, r_val: int = 3,
+                     nan_pattern: str = "({0} IS NULL OR LOWER(CAST({0} AS VARCHAR(50))) = 'nan')",
+                     preprocessing: Optional[str] = None):
+    """
+    Get encoding case when for category
+
+    Args:
+        woe_dict:
+        name:
+        r_val:
+        nan_pattern:
+        preprocessing:
+
+    Returns:
+
+    """
+
+    # value in case
+    f_val = name
+    if preprocessing is not None:
+        f_val = preprocessing.format(name)
+
+    # search for NaN and Small encodings
+    nan_val, small_val, small_grp = None, None, None
+    for grp in woe_dict.split:
+        if type(grp) is str:
+            if grp.startswith('__NaN_'):
+                nan_grp = woe_dict.split[grp]
+                nan_val = round(woe_dict.cod_dict[nan_grp], r_val)
+
+            if grp.startswith('__Small_'):
+                small_grp = woe_dict.split[grp]
+                small_val = round(woe_dict.cod_dict[small_grp], r_val)
+
+    # search for small in cod_dict
+    for grp in woe_dict.cod_dict:
+        if type(grp) is str:
+            if grp.startswith('__NaN_'):
+                nan_val = round(woe_dict.cod_dict[grp], r_val)
+            if grp.startswith('__Small_'):
+                small_val = round(woe_dict.cod_dict[grp], r_val)
+                small_grp = -1
+
+    assert nan_val is not None, 'NaN encoding value does not exists in woe_dict'
+    assert small_val is not None, 'Small encoding value does not exists in woe_dict'
+
+    feature = """CASE\n"""
+    if nan_val != small_val:
+        nan_case = nan_pattern.format(f_val)
+        feature += """  WHEN {0} THEN {1}\n""".format(nan_case, nan_val)
+
+    # create regular bins
+    passed = {small_grp}
+    for grp in woe_dict.split.values():
+        if grp not in passed:
+
+            search_vals = [x for x in woe_dict.split if woe_dict.split[x] == grp
+                           and x not in {"__NaN_0__", "__NaN_maxfreq__", "__NaN_maxp__",
+                                         "__NaN_minp__", "__Small_0__", "__Small_maxfreq__",
+                                         "__Small_maxp__", "__Small_minp__", "__NaN__", "__Small__"}]
+            length = len(search_vals)
+            search_vals = list(map(check_cat_symb, search_vals))
+
+            # filter NaN and Small cases separatly
+            enc_val = round(woe_dict.cod_dict[grp], r_val)
+            if length > 1:
+                feature += """  WHEN {0} IN ({1}) THEN {2}\n""".format(f_val, ', '.join(search_vals), enc_val)
+            elif length == 1:
+                feature += """  WHEN {0} = {1} THEN {2}\n""".format(f_val, search_vals[0], enc_val)
+
+            passed.add(grp)
+
+    # create last ELSE with small
+    feature += """  ELSE {1}\nEND AS {0}""".format(name, small_val, )
+
+    return feature
+
+
+def set_indent(x: str, n: int = 2):
+    """
+
+    Args:
+        x:
+        n:
+
+    Returns:
+
+    """
+    indent = ' ' * n
+
+    x = indent + x
+    x = x.replace('\n', '\n' + indent)
+
+    return x
+
+
+def get_encoded_table(model, table_name, round_woe=3, round_features=5,
+                      nan_pattern_numbers="({0} IS NULL OR {0} = 'NaN')",
+                      nan_pattern_category="({0} IS NULL OR LOWER(CAST({0} AS VARCHAR(50))) = 'nan')",
+                      preprocessing=None):
+    """
+    Get encoding table
+
+    Args:
+        model:
         table_name:
+        round_woe:
+        round_features:
+        nan_pattern_numbers:
+        nan_pattern_category:
+        preprocessing:
 
     Returns:
 
     """
-    result = ['SELECT']
+    if preprocessing is None:
+        preprocessing = {}
 
-    str_type = 'VARCHAR(50)'
+    query = """SELECT\n"""
 
-    for i, feature in enumerate(model_data['features']):
-        woe = model_data['features'][feature]
+    for n, name in enumerate(model.features_fit.index):
 
-        if woe['f_type'] == 'real':
-            string = f"(CASE WHEN {feature} IS NULL OR {feature} = 'NaN' THEN NULL ELSE {feature} END) AS {feature}"
+        woe_dict = model.woe_dict[name]
+
+        prep = None
+        if name in preprocessing:
+            prep = preprocessing[name]
+
+        if woe_dict.f_type == 'cat':
+            feature = prepare_category(woe_dict, name, round_woe, nan_pattern_category, prep)
         else:
-            big_cats, spec_value = woe['spec_cat']
-            big_cats_str = ','.join(map(lambda x: f"'{x}'", big_cats))
-            string = f"""(CASE WHEN {feature} IS NULL OR LOWER(CAST({feature} AS {str_type})) = 'nan' 
-THEN NULL WHEN {feature} NOT IN ({big_cats_str}) THEN '{spec_value}' ELSE CAST({feature} AS {str_type}) END) AS {feature}"""
+            feature = prepare_number(woe_dict, name, round_woe, round_features, nan_pattern_numbers, prep)
 
-        is_not_final = i != len(model_data['features']) - 1
-        result.append('    ' + string + (',' if is_not_final else ''))
+        query += set_indent(feature)
 
-    result.append(f'FROM {table_name}')
-    return result
+        if (n + 1) != len(model.features_fit):
+            query += ','
+
+        query += '\n'
+
+    query += """FROM {0}""".format(table_name)
+
+    return query
 
 
-def transform_features(model_data: Dict, from_source: List[str]) -> List[str]:
+def get_weights_query(model, table_name, output_name='PROB', alias='WOE_TAB', bypass_encoded=False, round_wts=3):
     """
+    Calc prob over woe table
 
     Args:
-        model_data:
-        from_source:
-
-    Returns:
-
-    """
-    result = ['SELECT']
-
-    for i, feature in enumerate(model_data['features']):
-        woe = model_data['features'][feature]
-        nan_value = woe['nan_value']
-
-        if woe['f_type'] == 'real':
-            if len(woe['splits']) == 0:
-                string = f"{woe['cod_dict'][0]}"
-            else:
-                splits = [(woe['splits'][i], woe['cod_dict'][i]) for i in range(len(woe['splits']))]
-                map_str = ' '.join([f'WHEN {feature} <= {x[0]} THEN {x[1]}' for x in splits])
-                else_case = woe['cod_dict'][len(woe['splits'])]
-                nan_case = woe['spec_cod'][nan_value] if nan_value in woe['spec_cod'] else 0.0
-                string = f"CASE WHEN {feature} IS NULL THEN {nan_case} {map_str} ELSE {else_case} END"
-        else:
-            if len(woe['cat_map']) == 0:
-                string = f"{woe['cod_dict'][0]}"
-            else:
-                cat_map = [(cat, woe['cod_dict'][woe_idx]) for cat, woe_idx in woe['cat_map'].items()]
-                map_str = ' '.join([f"WHEN {feature} = '{x[0]}' THEN {x[1]}" for x in cat_map])
-                spec_case = ' '.join(map(lambda x: f"WHEN {feature} = '{x[0]}' THEN {x[1]}", woe['spec_cod'].items()))
-                if nan_value in woe['spec_cod']:
-                    nan_case = woe['spec_cod'][nan_value]
-                elif nan_value in woe['cat_map']:
-                    nan_case = woe['cod_dict'][woe['cat_map'][nan_value]]
-                else:
-                    nan_case = 0.0
-                string = f"CASE WHEN {feature} IS NULL THEN {nan_case} {spec_case} {map_str} ELSE {nan_case} END"
-
-        is_not_final = i != len(model_data['features']) - 1
-        result.append(f"    ({string}) AS {feature}" + (',' if is_not_final else ''))
-
-    result.append(f'FROM (')
-
-    for s in from_source:
-        result.append(f'    {s}')
-
-    result.append(f')')
-    return result
-
-
-def predict_proba(model_data: Dict, from_source: List[str]) -> List[str]:
-    """
-
-    Args:
-        model_data:
-        from_source:
-
-    Returns:
-
-    """
-    result = ['SELECT']
-
-    sum_of_feats = ' + '.join(map(lambda x: f"({x[0]})*({x[1]['weight']})", model_data['features'].items()))
-    result.append(f"    1/(1+EXP(-(({sum_of_feats})+({model_data['intercept']})))) AS result")
-
-    result.append(f'FROM (')
-
-    for s in from_source:
-        result.append(f'    {s}')
-
-    result.append(f')')
-    return result
-
-
-def get_sql_query(model_data: Dict, table_name: str) -> str:
-    """
-
-    Args:
-        model_data:
+        model:
         table_name:
+        output_name:
+        alias:
+        bypass_encoded:
+        round_wts:
 
     Returns:
 
     """
-    buffer = StringIO()
-    feats = preprocess_features(model_data, table_name)
-    woe_features = transform_features(model_data, feats)
-    result_sql = predict_proba(model_data, woe_features)
+    query = """SELECT\n  1 / (1 + EXP(-({0}\n  ))) as {3}{1}\nFROM {2} as {4}"""
 
-    for line in result_sql:
-        buffer.write(line)
-        buffer.write('\n')
+    dot = '\n    {0}'.format(round(model.intercept, round_wts))
 
-    result = buffer.getvalue()
-    buffer.close()
-    return result
+    for name, val in zip(model.features_fit.index, model.features_fit.values):
+        sign = '' if val < 0 else '+'
+        dot += """\n    {0}{1}*{3}.{2}""".format(sign, round(val, round_wts), name, alias)
+
+    other = ''
+    if bypass_encoded:
+        other = """,\n  {0}.*""".format(alias)
+
+    return query.format(dot, other, table_name, output_name, alias)
+
+
+def get_sql_inference_query(model, table_name, round_digits=3, round_features=5, output_name='PROB', alias='WOE_TAB',
+                            bypass_encoded=True, template=None,
+                            nan_pattern_numbers="({0} IS NULL OR {0} = 'NaN')",
+                            nan_pattern_category="({0} IS NULL OR LOWER(CAST({0} AS VARCHAR(50))) = 'nan')",
+                            preprocessing=None):
+    """
+    Get sql query
+
+    Args:
+        model:
+        table_name:
+        round_digits:
+        round_features:
+        output_name:
+        alias:
+        bypass_encoded:
+        template:
+        nan_pattern_numbers:
+        nan_pattern_category:
+        preprocessing:
+
+    Returns:
+
+    """
+    assert template in ['td'] or template is None, 'Unknown template'
+
+    if template == 'td':
+        nan_pattern_numbers = '{0} IS NULL'
+        nan_pattern_category = '{0} IS NULL'
+
+    # get table with features
+    encode_table = "({0})".format(get_encoded_table(model, table_name, round_digits, round_features,
+                                                    nan_pattern_numbers, nan_pattern_category, preprocessing))
+    encode_table = """\n  """ + set_indent(encode_table)
+
+    # get table with weights
+    query = get_weights_query(model, encode_table, output_name=output_name,
+                              bypass_encoded=bypass_encoded, alias=alias, round_wts=round_digits)
+
+    return query
